7698391e96d772c3ef9b24819d026e540c9e8236
Donnie Brown <dpbrownjr@gmail.com>
2022-02-25 10:17:59 -0500

    added calc.minus()
    added calc.minus() and javadoc comments

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -1,7 +1,25 @@
 package travis_ci_tutorial_java;
 
 public class SimpleCalculator {
+	
+	/**
+	 * Adds two numbers, returning the sum
+	 * @param a addend
+	 * @param b addend
+	 * @return sum of the two addends
+	 */
 	public int add(int a, int b) {
 		return a + b;
+	}
+	
+	
+	/**
+	 * Subtracts two numbers, returning the difference
+	 * @param a minuend
+	 * @param b subtrahend
+	 * @return difference of minuend - subtrahend
+	 */
+	public int minus(int a, int b) {
+		return a - b;
 	}
 }
